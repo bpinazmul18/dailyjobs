c918acba77cf182713816d0dc33e9479722012f2
Nazmul haque <bpinazmul18@gmail.com>
2022-12-04 11:06:28 +0600

    Practice with wildcard endPoint and route constraints.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/hello', function () {
     return 'Hello world';
 });
+
+Route::get('/posts/{id}', function ($id) {
+    return response('Posts' . $id);
+})->where('id', '[0-9]+');
