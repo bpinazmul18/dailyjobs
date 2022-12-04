ee176f6d64790fe86d55c6d01221b17a92fd2bd1
Nazmul haque <bpinazmul18@gmail.com>
2022-12-04 15:51:22 +0600

    Blade template and Basic directives.

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,13 @@
+<h1>{{ $heading }}</h1>
+
+@unless(count($listings) == 0)
+
+@foreach($listings as $listing)
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+@endforeach
+
+@else
+    <p>No listing found!</p>
+
+@endunless
