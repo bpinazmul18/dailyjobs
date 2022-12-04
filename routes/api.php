0faf72b482974140d215a39d64e84ce1f5419116
Nazmul haque <bpinazmul18@gmail.com>
2022-12-04 12:54:14 +0600

    API routes.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', function () {
+    return response()-> json([
+        'posts' => [
+            'title' => 'Title one',
+        ],
+        'videos' => [
+            'title' => 'Video one',
+        ]
+    ]);
+});
