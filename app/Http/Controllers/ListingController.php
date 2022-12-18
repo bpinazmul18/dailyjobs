ceef97dd3e4d971e1c7ef63a367726b149fa2af2
Nazmul haque <bpinazmul18@gmail.com>
2022-12-18 16:58:52 +0600

    Validating and storing list.

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -24,5 +25,26 @@ class ListingController extends Controller
     // Create single listing
     public function create(Listing $listing) {
         return view('listings.create');
+    }
+
+    // Store listing data
+    public function store(Request $request) {
+        $validateData = $request->validate([
+            'company' => [
+                'required',
+                Rule::unique('listings', 'company'),
+            ],
+            'title' => 'required',
+            'location'=> 'required',
+            'email' => [ 'required', 'email'],
+            'website'=> 'required',
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+        
+        // return dd($request->all());
+        // dd($validateData);
+
+        return redirect('/');
     }
 }
