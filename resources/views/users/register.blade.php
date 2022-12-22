29957ab29ef261bec35bdac3257e56d8f2c7f310
Nazmul haque <bpinazmul18@gmail.com>
2022-12-22 17:02:28 +0600

    Keep old value in register form.

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -13,7 +13,7 @@
                 <label for="name" class="inline-block text-lg mb-2">
                     Name
                 </label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name" />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name" value="{{ old("name")}}" />
 
                 @error('name')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -22,7 +22,7 @@
 
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">Email</label>
-                <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" />
+                <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{ old("email")}}" />
 
                 @error('email')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
