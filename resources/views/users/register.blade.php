f5f5382733d9a8d132fb57e0247c9aaa61c9ba72
Nazmul haque <bpinazmul18@gmail.com>
2022-12-22 15:00:55 +0600

    show error message.

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -15,15 +15,19 @@
                     Name
                 </label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name" />
+
+                @error('name')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">Email</label>
                 <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" />
-                <!-- Error Example -->
-                <p class="text-red-500 text-xs mt-1">
-                    Please enter a valid email
-                </p>
+
+                @error('email')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
@@ -31,13 +35,21 @@
                     Password
                 </label>
                 <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password" />
+
+                @error('password')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
-                <label for="password2" class="inline-block text-lg mb-2">
+                <label for="confirm_password" class="inline-block text-lg mb-2">
                     Confirm Password
                 </label>
-                <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password2" />
+                <input type="password" class="border border-gray-200 rounded p-2 w-full" name="confirm_password" />
+
+                @error('confirm_password')
+                <p class="text-red-500 text-xs mt-1">{{ $message}}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
