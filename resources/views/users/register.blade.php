007c935b887a96080065e6dc3c335171ef38354d
Nazmul haque <bpinazmul18@gmail.com>
2022-12-22 15:03:15 +0600

    Add link already have an account.

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -61,7 +61,7 @@
             <div class="mt-8">
                 <p>
                     Already have an account?
-                    <a href="login.html" class="text-laravel">Login</a>
+                    <a href="/users/login" class="text-laravel">Login</a>
                 </p>
             </div>
         </form>
